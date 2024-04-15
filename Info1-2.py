c7e9074bd9f3b0f7a5446349544442b42ce98cfe
juanloyolak <juanloyolaoficial@gmail.com>
2024-04-15 19:01:24 +0000

    Archivos de la Evaluación1

---
+++ Info1-2.py
@@ -0,0 +1,12 @@
+nombre = input("Cual es tu nombre? ")
+apellido = input("Cual es tu apellido? ")
+edad = input("Edad actual ")
+ciudad = input("En que ciudad vive? ")
+carrera = input("Cual es la carrera que estudia? ")
+año = input("¿Cuál es el año que está cursando actualmente? ")
+sede = input("¿Cual es la sede donde estudia? ")
+semestre = input("¿En que semestre se encuentra actualmente? ")
+asignatura = input("¿Asignatura de esta evaluación? ")
+
+
+print("Hola " + nombre, apellido + "! Tu ubicación es "+ ciudad +" tienes "+ edad +" años. " + "La carrera que estudias es " + carrera + " Estas actualmente en la asignaruta " + asignatura + " Estas en el semestre " + semestre + " En el año de tu carrera " + año + " En la sede " + sede)
---
+++ Info2-2.py
@@ -0,0 +1,6 @@
+VLANnormal = 1
+VLANextendida = 100
+if VLANnormal < VLANextendida:
+    print("La VLAN es de rango normal")
+else:
+    print("La VLAN es de rango extendida")
